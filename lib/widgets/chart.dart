36b43fc5a54495fa4df0ef4ccca8915818b9dd8e
reinhart <reinhartandreas96@gmail.com>
2019-11-19 16:16:43 +0700

    before refactor card to ListTile widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import 'package:expenses/models/transaction.dart';  //<Transaction> constructor()
+import 'package:expenses/widgets/chart_bar.dart';
 
 
 class Chart extends StatelessWidget {
@@ -14,10 +15,10 @@ class Chart extends StatelessWidget {
     // get = function is called each time when groupedTransactionValues has to be read from another part of the code.
     return List.generate(7,  (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index)); // 2019-11-17 22:14:05.295174
-      // print(weekDay); // 2019-11-17 22:14:05.295174 (7 lines on terminal because of 7 index)
+      print('weekday = $weekDay');  // 2019-11-17 22:14:05.295174 (7 lines on terminal because of 7 index)
       /*
         For each list item the related date is calculated:
-        today - 0 = today,
+        today - 0 = today,      // 2019-11-18 21:08:03.614830 (today is 18 Nov 2019)
         today - 1 = yesterday,
         ...
         today - 6 = 1 week ago
@@ -33,19 +34,42 @@ class Chart extends StatelessWidget {
       // print(DateFormat.E().format(weekDay));  // Today = Sunday -> Sun | Sat | Fri | Thu | Wed ...
       // print(totalSum);
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });  // length = 7, generator = function
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, index) {
+      return sum + index['amount'];
+    });
+    // fold(initialValue, combine);
   }
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
+    print('groupedTransactionValues = $groupedTransactionValues');
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: <Widget>[
-
-      ],),
+      child: Padding( // Padding also can be achieved with Container()
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map( (dayData) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label : dayData['day'],
+                spendingAmount: dayData['amount'],
+                spendingPercentageOfTotal : totalSpending == 0.0 ? 0.0 : (dayData['amount'] as double)/totalSpending
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
